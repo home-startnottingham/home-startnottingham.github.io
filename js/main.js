61c61053195b45c1f1b9f723000f6e405dca47a3
labiraus <oliver.hathaway@bjss.com>
2025-06-25 10:53:30 +0100

    Restored from backup

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 $(function() {
   $('#header').load("./header.html");
   $('#carousel').load("./carousel.html");
-  console.log('xxxxx load carousel done');
   $('#footer').load("./footer.html");
 });
 
